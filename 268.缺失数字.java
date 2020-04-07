6b87f4df0887b3acce969d12a11a9a862a9ae76f
anothu <814118555@qq.com>
2020-04-07 23:40:27 +0800

    first commit

---
+++ 268.缺失数字.java
@@ -0,0 +1,21 @@
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+/*
+ * @lc app=leetcode.cn id=268 lang=java
+ *
+ * [268] 缺失数字
+ */
+
+// @lc code=start
+class Solution {
+    public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != i)
+                return i;
+        }
+        return nums.length;
+    }
+}
+// @lc code=end
