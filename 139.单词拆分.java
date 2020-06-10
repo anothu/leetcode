e7db5885997244d87a01819c3b7057fa9bbf8475
anothu <814118555@qq.com>
2020-06-10 23:36:18 +0800

    first commit

---
+++ 139.单词拆分.java
@@ -0,0 +1,27 @@
+import java.util.HashSet;
+
+/*
+ * @lc app=leetcode.cn id=139 lang=java
+ *
+ * [139] 单词拆分
+ */
+
+// @lc code=start
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        return word_Break(s, new HashSet(wordDict), 0);
+    }
+    public boolean word_Break(String s, Set<String> wordDict, int start) {
+        if (start == s.length()) {
+            return true;
+        }
+        for (int end = start + 1; end <= s.length(); end++) {
+            if (wordDict.contains(s.substring(start, end)) && word_Break(s, wordDict, end)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+// @lc code=end
+
